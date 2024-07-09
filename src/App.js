e4278477c5fd772169f789e2f7c40586084a4cec
TienNguyen <nguyentien1668@gmail.com>
2024-07-09 21:30:22 +0700

    upload i18n

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useTranslation } from "react-i18next";
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t("Welcome to React")} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
