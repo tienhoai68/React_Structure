e5479a3740f93174d358ab08dcacbf3d3875d0a2
TienNguyen <nguyentien1668@gmail.com>
2024-07-10 13:57:37 +0700

    upload gitignore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useTranslation } from "react-i18next";
 function App() {
   const { t, i18n } = useTranslation();
+  
   return (
     <div className="App">
       <header className="App-header">
